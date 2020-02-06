c13a37fff473f639d2a24c230f64c2b0be02412c
Adam <adampoznanski@outlook.com>
2020-02-06 15:22:53 -0500

    bp to deploy to heroku

--- server/server.js
+++ server/server.js
@@ -3,6 +3,15 @@ const express = require('express')
 const app = express()
 
 
+//serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
 
-const port = 5000
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`app is up on port ${port}`))
