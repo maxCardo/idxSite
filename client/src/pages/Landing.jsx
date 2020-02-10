c0543960daabd68cddf5dcf1027f34b00a2aaae7
Ryan Guild <ryanwork489@gmail.com>
2020-02-10 14:01:28 -0500

    screwed up the landing page

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,17 +1,14 @@
 import React, { Fragment } from "react";
 import { CenteredContainer } from "../styled-components/Containers";
-import { SubTitle } from "../styled-components/TextElements";
 import SearchBar from "../components/SearchBar";
 
 /**
  * @type {React.FC}
  */
 const Landing = () => (
-  <ParalaxContainer>
-    <CenteredContainer>
-      <SearchBar />
-    </CenteredContainer>
-  </ParalaxContainer>
+  <CenteredContainer>
+    <SearchBar />
+  </CenteredContainer>
 );
 
 export default Landing;
